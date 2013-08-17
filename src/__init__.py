1f0c24909e8f87fa9b2db493ff8f213309f93f30
Thomas Pegot-Ogier <thomas.pegot@gmail.com>
2013-08-17 19:35:36 +0200

    Add Preproccessing sub-package and begin class Comment __init__.py

---
+++ src/__init__.py
@@ -0,0 +1,42 @@
+
+"""
+catarob
+=====
+
+Provides
+  1. An array object of arbitrary homogeneous items
+  2. Fast mathematical operations over arrays
+  3. Linear Algebra, Fourier Transforms, Random Number Generation
+
+How to use the documentation
+----------------------------
+We recommend exploring the docstrings using
+`IPython <http://ipython.scipy.org>`_, an advanced Python shell with
+TAB-completion and introspection capabilities.  See below for further
+instructions.
+
+The docstring examples assume that `catarob` has been imported as `cb`::
+
+  >>> import catarob as cp
+
+Use the built-in ``help`` function to view a function's docstring::
+
+  >>> help(cb.lib.cvnumpy)
+  ... # doctest: +SKIP
+
+Available subpackages
+---------------------
+core
+    Core functions used by gui
+lib
+    Basic functions used by several sub-packages.
+gui
+    Graphical interface that use core functions
+"""
+
+import lib
+from lib import *
+import core
+import gui
+
+    
---
+++ src/core/__init__.py
@@ -0,0 +1,20 @@
+"""
+========================================
+core (:mod:`catarob.core`)
+========================================
+
+.. currentmodule:: catarob.core
+
+Sub-package for objects used in core.
+
+As listed below, this sub-package contains image processing functions and classes.
+
+Core Classes
+========================
+
+.. autosummary::
+   :toctree: generated/
+
+        Preprocessing
+   
+"""
